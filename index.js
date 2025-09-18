e7674e553109b867fd5ec5c092d8ae799ae96fd6
Rodney Swaji <rodney.swaji@student.moringaschool.com>
2025-09-19 01:22:42 +0300

    phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching (array, driverName) {
+    const match = array.filter(item => item === driverName || item === driverName.toLowerCase())
+    return match
+}
+function fuzzyMatch (array, firstLetter) {
+    const match = array.filter(item => item.slice(0,2) === firstLetter)
+    return match
+}
+function matchName (arrayOfDriverObject, driver_Name) {
+   
+    const nameMatch = arrayOfDriverObject.filter(item => item.name === driver_Name);
+    return nameMatch
+}
